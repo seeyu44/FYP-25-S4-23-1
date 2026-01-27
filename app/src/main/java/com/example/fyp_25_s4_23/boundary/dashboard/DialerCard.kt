289ac30841e473d24485c3ce383b7f37b19051db
seeyu44 <seeyu44@gmail.com>
2026-01-27 21:28:41 +0800

    Update DialerCard.kt

--- app/src/main/java/com/example/fyp_25_s4_23/boundary/dashboard/DialerCard.kt
+++ app/src/main/java/com/example/fyp_25_s4_23/boundary/dashboard/DialerCard.kt
@@ -4,8 +4,8 @@ import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Backspace
 import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -91,7 +91,7 @@ fun DialerCard() {
                             errorMessage = null
                         }) {
                             Icon(
-                                Icons.AutoMirrored.Filled.Backspace,
+                                Icons.Default.Clear,
                                 contentDescription = "Delete",
                                 tint = MaterialTheme.colorScheme.onSurfaceVariant
                             )
