3d21423af0e86243960896fc16a2eaad52ecd0c0
zaviergit17 <zavierng1712@gmail.com>
2026-01-28 12:02:44 +0800

    fixed some bugs in deepfake call monitoring

--- app/src/main/java/com/example/fyp_25_s4_23/boundary/dashboard/DialerCard.kt
+++ app/src/main/java/com/example/fyp_25_s4_23/boundary/dashboard/DialerCard.kt
@@ -51,7 +51,6 @@ fun DialerCard() {
             )
 
             Spacer(modifier = Modifier.height(16.dp))
-g
             Button(
                 onClick = {
                     if (targetUsername.isNotBlank()) {
